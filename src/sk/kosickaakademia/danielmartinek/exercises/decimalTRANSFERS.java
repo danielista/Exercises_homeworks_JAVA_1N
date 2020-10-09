397d29ecf6bea9240a3fb6a407c02c42643e9499
danielista <61905525+danielista@users.noreply.github.com>
2020-10-09 21:05:24 +0200

    UPDATE: TRANSFERS (homework) DONE

--- src/sk/kosickaakademia/danielmartinek/exercises/decimalTRANSFERS.java
+++ src/sk/kosickaakademia/danielmartinek/exercises/decimalTRANSFERS.java
@@ -1,100 +1,114 @@
 package sk.kosickaakademia.danielmartinek.exercises;
 import java.util.Scanner;
 
-// Na vstupe žiada číslo a hodnotu do akej sustavy chceme preložiť sustavu 2,8,16
-// vraj to robiť cez STRINGY :D lebo pri 16 už musime :D
-// takže ošetriť treba zle vstupy kým nezoberie dobrý vstup :D
-// vraj sa da aj farebne vypisovať do kozoly :DDDDDD
-//
+// APPLICATION which convert positive decimal number to selected number system (bin,oct or hexadec)
 
 
 public class decimalTRANSFERS {
     public static void main(String[] args) {
 
+
         Scanner skenerik = new Scanner(System.in);
         System.out.print("CONVERTOR FROM DECIMAL TO BINARY, OCTAL AND HEXADECIMAL: \n " +
                 "Please enter valid positive decimal number:");
 
+        boolean input = false;                                          // Ošetrenie zadávania vstupných hodnôt
+        int number = skenerik.nextInt();                                // DO WHILE loop
+            do if (number < 0) {
+                System.out.println("NEMOŽEŠ ZAPORNE ZADAVAŤ !!!  Please ENTER IT AGAIN ;)");
+                number = skenerik.nextInt();
+                input = false;
+            } else
+                input = true;
+            while (!input);                   // while input is not valid
 
-        int number = skenerik.nextInt();
-            do {
-                if (number<0){
-                    System.out.println("NEMOŽEŠ ZAPORNE ZADAVAŤ !!!");
-                    number = skenerik.nextInt();
+
+        do{
+            System.out.println("Please tap B for BINARY, O for OCTAL or H for HEXADECIMAL");
+            String system = skenerik.next();
+
+                switch (system) {
+                    case "B", "b" -> {
+                        input = true;
+                        System.out.println("Number in binary: " + binary(number));
+                    }
+                    case "o", "O" -> {
+                        input = true;
+                        System.out.println("Number in octal: " + octal(number));
+                    }
+                    case "H", "h" -> {
+                        input = true;
+                        System.out.print(hexadecimal(number));
+                    }
+                    default -> {
+                        input = false;
+                        System.out.println("POSNAŽ SA TRAFIŤ DO SPRÁVNEJ KLÁVESY");
+                        system = skenerik.next();
+                    }
                 }
-            }while ( number <0 );
+        }while (!input);
 
-
-        System.out.println("Please tap B for BINARY, O for OCTAL or H for HEXADECIMAL");
-        String system = skenerik.next();
-
-
-        switch(system) {
-             case "B": case "b" :
-                System.out.print(Integer.toBinaryString(number));
-                System.out.println("Number in binary: " + binary(number));
-                break;
-             case "o" : case "O" :
-                System.out.println("Number in octal: " + octal(number));
-                break;
-             case "H": case "h" :
-                System.out.print(Integer.toHexString(number));
-                System.out.print(hexadecimal(number));
-                break;
-        }
-
-
-
-
-
-
+        // System.out.print(Integer.toBinaryString(number));        // one line methods conversions to chosen system
+        // System.out.print(Integer.toOctalString(number));         // jednoriadkove metody na prevod do sustav
+        // System.out.print(Integer.toHexString(number));
 
 
 
     }
     //////////////////// TU KONCI MAIN METODAA //////////////////////
 
-    private static String hexadecimal(long number){
+    private static String hexadecimal(long number){               // METHOD which convert decimal to HEXAdeci (from long to String on input)
         String hex = " ";
-        while ( number>0 ){                              //
-            if (number%16 <= 9) {                        //
-                hex = hex + number%16;          //
-            }else{
+        while ( number>0 ){
+            if (number%16 <= 9) hex = hex + number % 16;                     // delenie 16 so zvyškom
+            else{
                 int pismenko = (int) (number%16);
-                switch(pismenko) {
-                    case 10:
-                        hex = hex + "A";
-                        break;
-                    case 11:
-                        hex = hex + "B";
-                        break;
-                    case 12:
-                        hex = hex + "C";
-                        break;
-                    case 13:
-                        hex = hex + "D";
-                        break;
-                    case 14:
-                        hex = hex + "E";
-                        break;
-                    case 15:
-                        hex = hex + "F";
-                        break;
-                }
-            }number /= 16;
+                // SWITCH case na ošetrenie pre každé písmenko
+                hex = switch (pismenko) {
+                    case 10 -> hex + "A";
+                    case 11 -> hex + "B";
+                    case 12 -> hex + "C";
+                    case 13 -> hex + "D";
+                    case 14 -> hex + "E";
+                    case 15 -> hex + "F";
+                    default -> hex;
+                };
+            }
+            number /= 16;
         }
        return reverse(hex);
-
     }
+    /* //////////////////////////////////////////////////////////////////
+    /////////OVELA JEDNODUCHSIA A CEZ POLE ROBENA METODA deciToHexade/////////
+    ///////////////////////////////////////////////////////////////////////////////
+    public static String toHex(int decimal){
+     int rem;
+     String hex="";
+     char hexchars[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+    while(decimal>0)
+     {
+       rem=decimal%16;
+       hex=hexchars[rem]+hex;
+       decimal=decimal/16;
+     }
+    return hex;
+}
+     */
 
-    private static String reverse(String otocmaprosim){
-        String vstupnyString = otocmaprosim;
 
-        String[] result = new String[vstupnyString.length()];
+    public static String reverse(String input) {
 
-        for (int i = 0; i < vstupnyString.length(); i++) result[i] = vstupnyString[vstupnyString.length() - i - 1];
+        if (input == null) {
+            return input;
+        }
 
-        System.out.println((String.valueOf(result)));
+        String output = "";
+
+        for (int i = input.length() - 1; i >= 0; i--) {
+            output = output + input.charAt(i);
+        }
+
+        return output;
     }
 
 
@@ -109,6 +123,24 @@ public class decimalTRANSFERS {
         }
         return reverse(octal);
     }
+    /*// TO ISTE decimalToOctal oveľa efektívnejšou metodou ;) čum a uč sa ;) :D //
+
+    public static String toOctal(int decimal){
+        int rem;                                                            //declaring variable to store remainder
+        String octal="";                                                    //declareing variable to store octal
+        char octalchars[]={'0','1','2','3','4','5','6','7'};                //declaring array of octal numbers
+        while(decimal>0)                                                    //writing logic of decimal to octal conversion
+        {
+           rem=decimal%8;
+           octal=octalchars[rem]+octal;
+           decimal=decimal/8;
+        }
+        return octal;
+    }
+     */
+
+
+
 
 
     private static long binary(long number) {
@@ -139,3 +171,35 @@ public class decimalTRANSFERS {
     }
 
 }
+
+/*
+░░░░░░░░░░░░▄▄▄▄▄▄▄▄▄▄▄▄▄░░░░░░░░░░░░
+░░░░░░▄▄████▀▀▀▀▀░░░░░░▀▀█▄▄░░░░░░░░░
+░░░▄██▀▀░░░░░░░░░░░░░░░░░░▀██▄░░░░░░░
+░░▄█▀░░░░░░░░░░░░░░░░░░░░░░░░▀█▄░░░░░
+░██░░░░░░░░░░░░░░░░░░░░░░░░░░░▀█▄░░░░
+██░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▀█▄░░░
+██░░░░░░░░░░░░░░░░░░░░░░░░░▄▄▄░░▀█░░░
+█░░░░░░░░░░░░░░░░░░░░░░░░░░▀██▄░░██░░
+█░░░░░████░░░░░░░░░░░░░░░░░░░░░░░░█▄░
+█░░░░░▀▀▀█░░░░░░░░░░░░░░░░░░░░░░░░██░
+█░░░░░░░░░░░░░░░░░░░░░░░▄▄▄▄█████▀░█▄
+█░░░░░░░░░░▄▄▄▄▄██████▀▀▀▀▀▀░░░░░░░██
+█░░░░▄█████▀▀▀▀▀░▄▄▄████░░░░░░░░░░░██
+██░░░░░░░░░░░░░░░░▀░░░░░░░░░░░░░░░░██
+▀█▄░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█▀
+░▀█░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▄█░
+░░██░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░██░
+░░░██░░░░░░░░░░░░░░░░░░░░░░░░░░░░▄█░░
+░░░░▀██▄░░░░░░░░░░░░░░░░░░░░░░▄▄█▀░░░
+░░░░░░▀██▄░░░░░░░░░░░░░░░░░▄▄█▀░░░░░░
+░░░░░░░░░▀██▄░░░░░░░░░░░▄▄█▀░░░░░░░░░
+░░░░░░░░░░░░▀██▄▄▄▄▄▄▄▄█▀░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░█░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░█░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░█░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░█░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░█░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░█░░░░░░░░░░░░░░░░░░
+
+ */
