087e01dc54acd30af22055d4fc6f4621dc00f1c8
danielista <61905525+danielista@users.noreply.github.com>
2020-10-16 16:35:21 +0200

    HomeWork 20201016: 1. SKOLA reťazec  - pridanie komentárov  - zefektívnenie syntax-u

--- src/sk/kosickaakademia/danielmartinek/exercises/homeworks/SKOLAkolaOLAlaA.java
+++ src/sk/kosickaakademia/danielmartinek/exercises/homeworks/SKOLAkolaOLAlaA.java
@@ -2,24 +2,23 @@ package sk.kosickaakademia.danielmartinek.exercises.homeworks;
 
 public class SKOLAkolaOLAlaA {
     public static void main(String[] args) {
-        String text = "SKOLA";
+        String text = "ŠKOLA";
         skolaVola(text);
     }
+
+    /*
+    Táto metóda mi trvala asi 50 minút :DD  a potom som ju pomocou návrhov od IntelliJ IDEA skrátil syntax na MIN
+    vypisuje vstupný reťazec v každom novom riadku o začiatočné písmenko menej
+     */
     public static void skolaVola(String text){
         int len = text.length();
-        int lines = len;
-
-        while(lines>0){
-            for (int i=0; i<len; i++ ){
-                if (len == lines) {
+        int lines = len;                                        // helpfull variable for printing lines
+        while(lines>0){                                         // while lenght of String will become zero
+            for (int i=0; i<len; i++ )                          // statement of one line
+                if (len == lines || i >= len - lines) {         // if you are on right line you can print string
                     System.out.print(text.charAt(i));
-                }else{
-                    if (i>=(len-lines)){
-                        System.out.print(text.charAt(i));
-                    }
                 }
-            }
-            System.out.println( );
+            System.out.println( );                              // next line
             lines--;
         }
     }
@@ -29,7 +28,7 @@ public class SKOLAkolaOLAlaA {
 
 
 /*
-public static void skolaVola(String text){
+public static void skolaVola(String text){        // pôvodná syntaxne neokresaná metóda
         int len = text.length();
         int lines = len;
 
