eae085640df9776b237711e42175288e5f48f4a6
danielista <61905525+danielista@users.noreply.github.com>
2020-10-16 10:06:53 +0200

    ZLOMKY uloha na hodine ekonomicka fakulta KOSICE Zapocet z countingBounces :D

--- src/sk/kosickaakademia/danielmartinek/exercises/countBOUNCE.java
+++ src/sk/kosickaakademia/danielmartinek/exercises/countBOUNCE.java
@@ -1,9 +1,48 @@
 package sk.kosickaakademia.danielmartinek.exercises;
 
+import java.util.Scanner;
+
 public class countBOUNCE {
     public static void main(String[] args) {
 
+        // NO neviem prečo mi nešiel skener.. spustil sa program
+        // ale double nejak nechcelo načítať...
 
 
+      /*  Scanner scenner = new Scanner(System.in);
+        System.out.println("Enter Height and a KOef of bouncing, in [centiMeters] please: ");
+        int height = scenner.nextInt();
+        double koeficient = scenner.nextDouble();
+       */
+
+        System.out.println("Result: " + countBounces(200,0.9));
+    }
+
+    public static int countBounces(int height, double koeficient){
+        int bounces = 0;
+        while (height > 100.00){
+            height *= koeficient;
+            bounces++;
+        }
+        return bounces;
     }
 }
+
+/*
+░░░░░░░░░░░░░░░░░░░░░░█████████
+░░███████░░░░░░░░░░███▒▒▒▒▒▒▒▒███
+░░█▒▒▒▒▒▒█░░░░░░░███▒▒▒▒▒▒▒▒▒▒▒▒▒███
+░░░█▒▒▒▒▒▒█░░░░██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██
+░░░░█▒▒▒▒▒█░░░██▒▒▒▒▒██▒▒▒▒▒▒██▒▒▒▒▒███
+░░░░░█▒▒▒█░░░█▒▒▒▒▒▒████▒▒▒▒████▒▒▒▒▒▒██
+░░░█████████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██
+░░░█▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒▒▒██
+░██▒▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒██▒▒▒▒▒▒▒▒▒▒██▒▒▒▒██
+██▒▒▒███████████▒▒▒▒▒██▒▒▒▒▒▒▒▒██▒▒▒▒▒██
+█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒████████▒▒▒▒▒▒▒██
+██▒▒▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██
+░█▒▒▒███████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██
+░██▒▒▒▒▒▒▒▒▒▒████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█
+░░████████████░░░█████████████████
+
+ */
