cc42c0dd24c65340ef483ee7ebd7d97f7763d698
danielista <61905525+danielista@users.noreply.github.com>
2020-10-16 14:54:54 +0200

    random GUESS the NUMBER (update)

--- src/sk/kosickaakademia/danielmartinek/exercises/myRANDOM.java
+++ src/sk/kosickaakademia/danielmartinek/exercises/myRANDOM.java
@@ -1,27 +1,67 @@
 package sk.kosickaakademia.danielmartinek.exercises;
-
 import java.util.Scanner;
 
+/*
+    GAME Guess my number:
+    - interactive game with inputs and hints
+    - added attempts counting
+    - added MAX attempts
+ */
+
 public class myRANDOM {
-    public static void main(String[] args) { //0,1465432 -> 14,153465
-      int max = 122;
+    public static void main(String[] args) {
+      int max = 80;
       guessNumber(max);
     }
 
     public static void guessNumber (int max){
-        int randomNUMBER = (int)(Math.random()*max + 1);  //100 daj max
+        int randomNUMBER = (int)(Math.random()*max + 1);
         Scanner skenerik = new Scanner(System.in);
         int guess;
-        int attempt = 0;
+        int attempt = 1;
+        int maxAttempt = 5;
+
+        System.out.println("Zadaj čislo kámo, a pokús sa uhádnuť moje číslo (0-" +max+ "): ");
+        System.out.println("Máš len " + maxAttempt + " pokusov. Tak sa snaž ;) ");
+
         do {
-            System.out.println("zadaj čislo kámo: ");
             guess=skenerik.nextInt();
             if(guess==randomNUMBER){
-                System.out.println("UHADOL SI ;)");
-            }else if (guess >= randomNUMBER){
-                System.out.println("skús vyššie číslo");
-            }else System.out.println("skús menšie číslo :D");
+                System.out.println("CONGRATULATIONS UHADOL SI HÁDANÉ ČÍSLO ;)  podarilo sa Ti to na " + attempt + " pokus.");
+                break;
+            }else if(attempt > maxAttempt-1) {
+                System.out.println("GAME OVER It is NOT even " + guess + " Presiahol si daný počet pokusov. try again later");
+                break;
+            }else if (guess <= randomNUMBER){
+                System.out.print("Hint: skús vyššie číslo");
+                attempt++;
+            }else if(guess >= randomNUMBER){
+                System.out.print("Hint: skús menšie číslo");
+                attempt++;
+            }
+            System.out.print("     ------> attempt number: " + attempt + "    ");
         }while(guess != randomNUMBER);
     }
 
 }
+
+
+/*
+┈┈╱▔▔▔▔▔╲┈┈┈HM┈HM
+┈╱┈┈╱▔╲╲╲▏┈┈┈HMMM
+╱┈┈╱━╱▔▔▔▔▔╲━╮┈┈
+▏┈▕┃▕╱▔╲╱▔╲▕╮┃┈┈
+▏┈▕╰━▏▊▕▕▋▕▕━╯┈┈
+╲┈┈╲╱▔╭╮▔▔┳╲╲┈┈┈
+┈╲┈┈▏╭━━━━╯▕▕┈┈┈
+┈┈╲┈╲▂▂▂▂▂▂╱╱┈┈┈
+┈┈┈┈▏┊┈┈┈┈┊┈┈┈╲┈
+┈┈┈┈▏┊┈┈┈┈┊▕╲┈┈╲
+┈╱▔╲▏┊┈┈┈┈┊▕╱▔╲▕
+┈▏ ┈┈┈╰┈┈┈┈╯┈┈┈▕▕
+┈╲┈┈┈╲┈┈┈┈╱┈┈┈╱┈╲
+┈┈╲┈┈▕▔▔▔▔▏┈┈╱╲╲╲▏
+┈╱▔┈┈▕┈┈┈┈▏┈┈▔╲▔▔
+┈╲▂▂▂╱┈┈┈┈╲▂▂▂╱┈
+
+ */
