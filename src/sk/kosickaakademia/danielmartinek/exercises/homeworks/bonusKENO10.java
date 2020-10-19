0abaa02b58eff0be6b6b6d0f324d8d88f1ba1607
danielista <61905525+danielista@users.noreply.github.com>
2020-10-19 08:54:29 +0200

    HomeWork 20201016: BONUS KENO 10  - aj s komentárom jedným  - všetko v maine :D  - robí to čo má ;)  - konečne som využil continue; .. yeah!!

---
+++ src/sk/kosickaakademia/danielmartinek/exercises/homeworks/bonusKENO10.java
@@ -0,0 +1,29 @@
+package sk.kosickaakademia.danielmartinek.exercises.homeworks;
+/*
+  _  ________ _   _  ____    ___   ___
+ | |/ /  ____| \ | |/ __ \  |__ \ / _ \
+ | ' /| |__  |  \| | |  | |    ) | | | |
+ |  < |  __| | . ` | |  | |   / /| | | |
+ | . \| |____| |\  | |__| |  / /_| |_| |
+ |_|\_\______|_| \_|\____/  |____|\___/
+ */
+public class bonusKENO10 {
+    public static void main(String[] args) {
+        int[] keno = new int[20];
+        System.out.println("V hre KENO 20 boli vyžrebované tieto čísla: ");
+        for (int i=0; i<20; i++){
+            keno[i] = (int)(Math.random()*80 + 1);
+
+            // checking the repeatable values, and their replacement
+            int j=0;
+            while (j<i) {
+                if (keno[i] == keno[j]) {
+                    keno[i]=(int)(Math.random()*80 + 1);
+                    continue;
+                }
+                j++;
+            }
+            System.out.print( keno[i] + "   ");
+        }
+    }
+}
