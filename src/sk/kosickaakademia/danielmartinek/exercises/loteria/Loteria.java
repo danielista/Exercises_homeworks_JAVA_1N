6783363e18d55a427bd8ae0b1adc33738d7d4532
danielista <61905525+danielista@users.noreply.github.com>
2020-10-20 16:26:30 +0200

    Exercise 20201020: Lottery (methods UPDATE)  - pridanie zadávania stávok  - jej ošetrenie vstupov  - pridanie metódy výpočtu výhry

--- src/sk/kosickaakademia/danielmartinek/exercises/loteria/Loteria.java
+++ src/sk/kosickaakademia/danielmartinek/exercises/loteria/Loteria.java
@@ -15,11 +15,15 @@ public class Loteria {
     private int[] zreb = new int[10];
     private int wins=0;
     private int[] winningNumbers = new int[5];
+    private double bet;
 
     public static void main(String[] args) {
-        System.out.println("\033[31;1m$$ Lott\033[32;1;2merry $$\033[0m");
-
+        System.out.println("\033[31;1m$$ WELCOME IN A Lott\033[32;1;2merry $$\033[0m");
         Loteria loteria = new Loteria();
+
+        // zadavanie stávky
+        loteria.enteringBet();
+
         // tipovanie 5-tich čísel a ich výpis a aj kontrola
         loteria.inputNumbers();
         // žrebovanie 10 čísel a ich výpis
@@ -27,6 +31,33 @@ public class Loteria {
         // kontrola počtu uhádnutých a ich výpis
         loteria.checkWinningNumbers();
 
+        // cashier banka vypočíta výhru :_D
+        loteria.cashWin(loteria.wins);
+
+    }
+
+
+    private void enteringBet() {
+        do{ System.out.println("Please enter your BET");
+            Scanner sc = new Scanner(System.in);
+            bet = sc.nextDouble();
+        } while ( bet<0 || bet == 0);
+    }
+
+
+
+    public void cashWin(int howManyWins){
+        double howMany = switch (howManyWins) {
+            case 1, 0 -> 0;
+            case 2 -> 2 * bet;
+            case 3 -> 15 * bet;
+            case 4 -> 500 * bet;
+            case 5 -> 10000 * bet;
+            default -> 0;
+        };
+        if (howManyWins>1)
+        System.out.println("You WON " + howMany + " $ [american dollars]");
+        else System.out.println("You won NOTHING bro... just try again ;$");
     }
 
 
@@ -39,15 +70,16 @@ public class Loteria {
                     winningNumbers[i]=zreb[j];
             }
         }
-        System.out.println("Congratulation you won "+wins+ " numbers.");
-        System.out.println("The numbers were: ");
+        System.out.print("Congratulation you won "+wins+ " numbers.");
+        System.out.print("The numbers were: ");
         if (winningNumbers == null) {
-            System.out.println("no žiadne si neuhádol skús znova :D");
+            System.out.println("no bohužiaľ, žiadne si neuhádol skús znova :D");
         }
         for (int i = 0; i < 5; i++) {
             if (winningNumbers[i] != 0 )
             System.out.print(winningNumbers[i] + " ");
         }
+        System.out.println();
     }
 
 
@@ -79,7 +111,7 @@ public class Loteria {
         int in;
         Scanner sc = new Scanner(System.in);
         while (i<5){
-            System.out.print("Enter"+ (i+1)+". number: ");
+            System.out.print("Enter "+ (i+1)+". number: ");
             in = sc.nextInt();
             if(in>0 && in<21 && checkArray(i,in)){
                 tip[i]=in;
