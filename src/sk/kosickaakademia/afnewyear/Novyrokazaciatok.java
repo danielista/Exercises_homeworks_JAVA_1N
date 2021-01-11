a4ef9f2756ad0d747cf4409d299ba919831faa9a
danielista <61905525+danielista@users.noreply.github.com>
2021-01-11 12:19:16 +0100

    Exercise 20210111: BALAST

--- src/sk/kosickaakademia/afnewyear/Novyrokazaciatok.java
+++ src/sk/kosickaakademia/afnewyear/Novyrokazaciatok.java
@@ -2,9 +2,28 @@ package sk.kosickaakademia.afnewyear;
 
 public class Novyrokazaciatok {
     public static void main(String[] args) {
-        System.out.println(numberWithTheMostOfDivisors(1000)); //840 s 32 delitelmi
-    }
+      //  System.out.println(numberWithTheMostOfDivisors(1000)); //840 s 32 delitelmi
 
+        String balast = "BBALASTBALASTRBALASTABALASTBALASTBALASTTBALASTBALASTIBALASTBALASTBALASTSLBALASTBALASTBALASTABALASTBALASTVBALASTBALASTA\n";
+        String balast2 = "BALASTBBABALASTLASTALASBALASTBALBALASTASTTSBALASTTABILIBALASTBALASTTABALASBALASTT\n";
+        System.out.println(balast.replaceAll("BALAST",""));
+
+         balast2 = balast2.replaceAll("BALAST","");
+         balast2 = balast2.replaceAll("BALAST","");
+         balast2 = balast2.replaceAll("BALAST","");
+
+       /* while (balast2.contains("_")==true){
+            balast2=balast2.replaceAll("______","");
+         //   boolean isFound = balast2.indexOf("BALAST") !=-1? true: false;
+         //   if (isFound == true){
+                balast2 = balast2.replaceAll("BALAST","______");
+                System.out.println(balast2.replaceAll("BALAST","______"));
+         //   }
+        }
+         */
+        System.out.println(balast2);
+
+    }
 
     public static int numberWithTheMostOfDivisors(int tisic) {
         int divisors = 0;
